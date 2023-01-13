c478c589639a658a4d348e8e3f136e835e180ddd
Grant Soper <granttravissoper@gmail.com>
2023-01-14 07:51:41 +1100

    First commit

---
+++ classes/ProductRate.py
@@ -0,0 +1,10 @@
+class ProductRate():
+
+    def __init__(self, id, product_id, rate_type_id, rate) -> None:
+        self.id = id
+        self.product_id = product_id
+        self.charge_type_id = rate_type_id
+        self.rate = rate
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.name}({vars(self)})'
