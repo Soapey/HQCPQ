d4357cc75e5bcfb90f86ca8d10140a0d91ebb09e
Grant Soper <granttravissoper@gmail.com>
2023-01-19 15:10:07 +1100

    -a

--- app/core.py
+++ app/core.py
@@ -6,31 +6,6 @@ from .db import SQLCursor
 import sys
 
 
-def test():
-
-    from app.classes.Quote import Quote
-    from app.classes.QuoteItem import QuoteItem
-    from datetime import datetime
-
-    SQLCursor.build_name = 'production'
-    
-    start(SQLCursor.build_name, True)
-
-    q = Quote(None, datetime.today(), datetime.today(), 'Grant Soper', '4 Bilmark Drive', 'Raymond Terrace', '0481552395')
-    q.insert()
-
-    i1 = QuoteItem(None, q.id, 'Truck', 12.5, 10, 'CMS', 14)
-    i1.insert()
-
-    i2 = QuoteItem(None, q.id, 'Trailer', 20, 10, '40mm Scalps', 10)
-    i2.insert()
-
-    print(len(QuoteItem.get()))
-
-    q.delete()
-
-    print(len(QuoteItem.get()))
-
 def main():
 
     SQLCursor.build_name = 'production'
