e7897ed68c1de4c0b7ed8201772d740d81539a74
Grant Soper <granttravissoper@gmail.com>
2023-04-26 12:30:53 +1000

    -a

--- hqcpq/hqcpq.py
+++ hqcpq/hqcpq.py
@@ -1,6 +1,4 @@
-import os
 import sys
-import shutil
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from hqcpq.gui.components.main_window import Ui_MainWindow
 from hqcpq.db.build_type_enum import BuildType
@@ -16,9 +14,7 @@ from hqcpq.gui.actions.ratetype_actions import connect as connect_rate_types
 from hqcpq.gui.actions.vehiclecombination_actions import (
     connect as connect_vehicle_combinations,
 )
-from hqcpq.helpers import read_config, update_config
-from tkinter import messagebox
-from tkinter.messagebox import askyesno
+from hqcpq.helpers import read_config
 
 
 def connect_main_window_actions(main_window: Ui_MainWindow):
@@ -77,94 +73,6 @@ def main():
     start_app()
 
 
-def update():
-
-    # Read configuration file.
-    config = read_config()
-    current_version = config["AppSettings"]["version"]
-    update_directory = config["AppSettings"]["update_directory"]
-
-    # Read update configuration file.
-    try:
-        update_config_file = read_config(f"{update_directory}\\update_config.ini")
-        update_version = update_config_file["Configuration"]["version"]
-        update_exe_name = update_config_file["Configuration"]["exe_name"]
-    except:
-        return
-
-    # Check if current app version out of date.
-    if current_version != update_version:
-
-        # Ask user if they would like to update.
-        if askyesno(
-            "Update Required",
-            "The current application version is out of date.\nWould you like to fetch the updated version?",
-        ):
-
-            try:
-                # Copy the new .exe file from the update directory to the current directory.
-                new_exe_path = os.path.join(os.path.abspath("."), update_exe_name)
-                shutil.copyfile(
-                    rf"{update_directory}\{update_exe_name}",
-                    new_exe_path,
-                )
-            except:
-                if os.path.exists(new_exe_path):
-                    os.remove(new_exe_path)
-
-                messagebox.showerror(
-                    "Update Failed",
-                    "An error occurred while trying to download the update file.\nPlease try again later.",
-                )
-                return
-
-            try:
-                # Change the current version in the config file.
-                config.set("AppSettings", "version", update_version)
-                update_config(config)
-            except:
-                messagebox.showerror(
-                    "Update Failed",
-                    "An error occurred while trying change the version number in the configuration file. Please change this manually.",
-                )
-                return
-
-            # Start the updated version.
-            os.startfile(new_exe_path)
-
-            # Exit the current (older) version.
-            sys.exit()
-
-
-def remove_previous_versions():
-
-    # Read configurationf file.
-    config = read_config()
-    base_exe_name = config["AppSettings"]["base_exe_name"]
-    current_version = config["AppSettings"]["version"]
-
-    # Loop through files in directory and remove any .exe that does not match current version.
-    try:
-        cwd = os.path.abspath(".")
-        for filename in os.listdir(cwd):
-
-            full_path = os.path.join(cwd, filename)
-            extension = os.path.splitext(filename)[-1]
-
-            if (
-                os.path.isfile(full_path)
-                and filename != f"{base_exe_name}_{current_version}.exe"
-                and extension == ".exe"
-            ):
-                os.remove(full_path)
-    except:
-        pass
-
-
 if __name__ == "__main__":
-
-    update()
-
-    remove_previous_versions()
 
     main()
