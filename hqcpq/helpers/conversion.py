ebc676182aa11542628f2e0dea0dc3fb0f8f9775
Grant Soper <granttravissoper@gmail.com>
2023-04-28 10:20:36 +1000

    Conform hqcpq.helpers.conversion to PEP conventions.

--- hqcpq/helpers/conversion.py
+++ hqcpq/helpers/conversion.py
@@ -3,15 +3,42 @@ from hqcpq.helpers.comparison import can_be_float
 
 
 def string_to_int(value: str) -> int:
+    """
+    Converts a string to an integer if possible.
+
+    Args:
+        value (str): The string value to be converted.
+
+    Returns:
+        int: The integer representation of the string if conversion is possible, else None.
+    """
     return int(value) if value.isnumeric() else None
 
 
 def string_to_float(value: str) -> float:
+    """
+    Converts a string to a float if possible.
+
+    Args:
+        value (str): The string value to be converted.
+
+    Returns:
+        float: The float representation of the string if conversion is possible, else None.
+    """
     return float(value) if can_be_float(value) else None
 
 
-def string_to_date(date_str, formats: list = None):
+def string_to_date(date_str: str, formats: list[str] = None) -> datetime.date:
+    """
+    Converts a string to a date if possible.
 
+    Args:
+        date_str (str): The string value to be converted to date.
+        formats (list[str], optional): A list of date formats to try for conversion. Defaults to None.
+
+    Returns:
+        datetime.date: The datetime.date object if the string can be converted, else None.
+    """
     if not formats:
         formats = ['%d/%m/%Y', '%d/%m/%y']
 
