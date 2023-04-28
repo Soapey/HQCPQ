3940084a02f944388a3327f696a8ba731abbb65d
Grant Soper <granttravissoper@gmail.com>
2023-04-28 10:17:30 +1000

    Conform hqcpq.helpers.comparison to PEP conventions.

--- hqcpq/helpers/comparison.py
+++ hqcpq/helpers/comparison.py
@@ -1,22 +1,40 @@
-from datetime import datetime
+import datetime
 
 
-def can_be_float(value: str):
+def can_be_float(value: str) -> bool:
+    """
+    Determines whether the given value can be parsed as a float.
+
+    Args:
+        value (str): The input string to be checked.
+
+    Returns:
+        bool: True if the input value can be parsed as a float, False otherwise.
+    """
     try:
-        f = float(value)
+        float(value)
         return True
-    except:
+    except ValueError:
         return False
 
 
-def can_be_date(value: str, formats: list = None):
+def can_be_date(value: str, formats: list = None) -> bool:
+    """
+    Determines whether the given value can be parsed as a date using the specified date formats.
 
+    Args:
+        value (str): The input string to be checked.
+        formats (list, optional): A list of date formats to try. Defaults to ['%d/%m/%Y', '%d/%m/%y'].
+
+    Returns:
+        bool: True if the input value can be parsed as a date using any of the specified date formats, False otherwise.
+    """
     if not formats:
         formats = ['%d/%m/%Y', '%d/%m/%y']
-        
-    for format in formats:
+    
+    for date_format in formats:
         try:
-            datetime.strptime(value, format)
+            datetime.datetime.strptime(value, date_format)
             return True
         except ValueError:
             pass
