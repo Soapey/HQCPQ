7ef3e71cd021536b30852ab43107bacd15894409
Grant Soper <granttravissoper@gmail.com>
2023-07-06 10:25:50 +1000

    -a

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import sys
+from tkinter import Tk
+
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from hqcpq.gui.components.main_window import Ui_MainWindow
+
+from hqcpq.db.SQLiteUtil import initialise_db
+from hqcpq.gui.actions.product_actions import connect as connect_products
+from hqcpq.gui.actions.productrate_actions import connect as connect_product_rates
 from hqcpq.gui.actions.quote_actions import (
     connect as connect_quotes,
     navigate_to_listing_view,
 )
 from hqcpq.gui.actions.quoteitem_actions import connect as connect_quote_items
-from hqcpq.gui.actions.product_actions import connect as connect_products
-from hqcpq.gui.actions.productrate_actions import connect as connect_product_rates
 from hqcpq.gui.actions.ratetype_actions import connect as connect_rate_types
+from hqcpq.gui.actions.transportsettings_actions import connect as connect_transport_settings
 from hqcpq.gui.actions.vehiclecombination_actions import (
     connect as connect_vehicle_combinations,
 )
-from hqcpq.gui.actions.transportsettings_actions import connect as connect_transport_settings
-from hqcpq.db.SQLiteUtil import initialise_db
+from hqcpq.gui.components.main_window import Ui_MainWindow
 
 
 def connect_main_window_actions(main_window: Ui_MainWindow):
@@ -32,6 +35,8 @@ def main():
     app = QApplication(sys.argv)
 
     main_window_root = QMainWindow()
+    tk_root = Tk()
+    tk_root.withdraw()
 
     main_window = Ui_MainWindow()
     main_window.setupUi(main_window_root)
