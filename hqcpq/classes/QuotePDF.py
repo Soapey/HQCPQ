529b1e0b31898778dd8313fbc8c8b09f58cd47db
Soapey <granttravissoper@gmail.com>
2024-05-15 16:41:32 +1000

    -a

--- hqcpq/classes/QuotePDF.py
+++ hqcpq/classes/QuotePDF.py
@@ -9,12 +9,8 @@ from hqcpq.helpers.io import join_to_project_folder
 from hqcpq.helpers.general import select_directory, insert_newline_at_max_length
 
 
-
-
-
 class QuotePDF(FPDF):
     def __init__(self, quote):
-
         super().__init__()
 
         self.quote = quote
@@ -30,18 +26,7 @@ class QuotePDF(FPDF):
         self._body()
         self._footer()
 
-    def adjust_font_size(self, cell_width_mm, datum, max_characters):
-        font_size = 8  # Initial font size
-
-        if len(datum) > max_characters:
-            overflow = len(datum) - max_characters
-            increments = overflow // 5
-            font_size -= 1.1 * increments
-
-        return font_size
-
     def _header(self):
-
         # Header
         self.set_font("Helvetica", "B", 20)
         self.cell(self.column_width_mm * 2, self.row_height_mm, "QUOTE")
@@ -64,7 +49,6 @@ class QuotePDF(FPDF):
         self.ln(20)
 
     def _body(self):
-
         # Supplier details.
         self.set_font("Helvetica", "B", 14)
         self.cell(self.column_width_mm * 2, self.row_height_mm, "Hunter Quarries")
@@ -172,7 +156,6 @@ class QuotePDF(FPDF):
         self.ln(self.row_height_mm)
 
         # QuoteItems
-        max_quote_item_character_width = 35
         self.set_font("Helvetica", "", 8)
         data = [
             [
@@ -184,13 +167,36 @@ class QuotePDF(FPDF):
             for quote_item in self.items.values()
         ]
 
-        # QuoteItems
+        # Calculate row heights
+        row_heights = []
+        for row in data:
+            max_height = 0
+            for col_index, col_text in enumerate(row):
+                # Temporarily set the y position to calculate the height
+                self.set_xy(self.get_x(), self.get_y())
+                if col_index == 1:  # 'Product' column which tends to have more text
+                    lines = self.multi_cell(self.column_width_mm, self.row_height_mm * 0.75, col_text, border=0,
+                                            align="L", split_only=True)
+                    num_lines = len(lines)
+                    max_height = max(max_height, self.row_height_mm * 0.75 * num_lines)
+                else:
+                    lines = self.multi_cell(self.column_width_mm, self.row_height_mm, col_text, border=0, align="L",
+                                            split_only=True)
+                    num_lines = len(lines)
+                    max_height = max(max_height, self.row_height_mm * num_lines)
+            row_heights.append(max_height)
+
+        # Render rows with calculated heights
         for row_index, row in enumerate(data):
-            for datum in row:
-                font_size = self.adjust_font_size(self.column_width_mm, datum, 35)
-                self.set_font("Helvetica", "", font_size)
-                self.cell(self.column_width_mm, self.row_height_mm, datum, border=1, align="L")
-            self.ln()
+            max_height = row_heights[row_index]
+            x_start = self.get_x()
+            y_start = self.get_y()
+            for col_index, col_text in enumerate(row):
+                self.set_xy(x_start + self.column_width_mm * col_index, y_start)
+                self.multi_cell(self.column_width_mm, max_height / len(
+                    self.multi_cell(self.column_width_mm, self.row_height_mm, col_text, border=0, align="L",
+                                    split_only=True)), col_text, border=1, align="L")
+            self.set_y(y_start + max_height)  # Ensure the next row starts immediately after the current row
 
         self.ln()
 
@@ -220,7 +226,6 @@ class QuotePDF(FPDF):
             )
 
     def _footer(self):
-
         # Business details.
         self.set_font("Helvetica", "B", 10)
         self.multi_cell(
@@ -230,7 +235,6 @@ class QuotePDF(FPDF):
         )
 
     def export(self, notification=True, save_directory_path=None):
-
         # Dialog box requests user to select destination folder.
         directory_path = save_directory_path
         if not directory_path:
